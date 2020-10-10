6c79767d9464c9f77bb0f8f669d200f12232d9c6
Tanish Sawant <tanishsawant@rediffmail.com>
2020-10-10 16:35:35 +0530

    models

--- pages/models.py
+++ pages/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class User(models.Model):
     user_name = models.CharField(max_length=200)
     password = models.CharField(max_length = 15)
-    profile_picture =  models.ImageField(default='default.png', upload_to='profile_pics')
-    
+    email = models.EmailField()
+    #profile_picture =  models.ImageField(default='default.png', upload_to='profile_pics')
