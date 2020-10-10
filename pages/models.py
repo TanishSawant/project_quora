4b4117a616f6bdb6f5e1332178b564d91605921b
Tanish Sawant <tanishsawant@rediffmail.com>
2020-10-10 16:31:18 +0530

    models of user

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    user_name = models.CharField(max_length=200)
+    password = models.CharField(max_length = 15)
+    profile_picture =  models.ImageField(default='default.png', upload_to='profile_pics')
+    
