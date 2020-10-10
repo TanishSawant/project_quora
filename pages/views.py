0ee8f43da1c202f3ae2848538a83bc5c00f7d736
Tanish Sawant <tanishsawant@rediffmail.com>
2020-10-10 17:13:59 +0530

    signup

--- pages/views.py
+++ pages/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import  django.template
 from django.shortcuts import render
+from .models import User
 
 def index(request):
     return render(request, "pages/homepage.html", {})
@@ -12,4 +13,11 @@ def loginPage(request) :
     return render(request, "pages/login.html", {})
 
 def signUpPage(request) :
+    print("--------------------------------------------------------")
+    print(request.POST)
+    print("-------------------------------------------")
+    username = request.POST.get('username')
+    emailid = request.POST.get('Email')
+    Password = request.POST.get('password')
+    User.objects.create(user_name = username, password = Password, email = emailid)
     return render(request, "pages/SignUp.html", {})
