b69335aea889aaa4e5cc9d445931d84da457aef2
Tanish Sawant <tanishsawant@rediffmail.com>
2020-10-14 18:59:59 +0530

    dashboard UI

--- pages/views.py
+++ pages/views.py
@@ -9,13 +9,13 @@ from plotly.offline import plot
 from plotly.graph_objs import Scatter
 
 def dashboardGraph(request):
-    x_data = [0,1,2,3]
+    x_data = [x for x in range(1000)]
     y_data = [x**2 for x in x_data]
     plot_div = plot([Scatter(x=x_data, y=y_data,
                         mode='lines', name='test',
                         opacity=0.8, marker_color='green')],
                         output_type='div')
-    return render(request, "dashboard.html", context={'plot_div': plot_div})
+    return render(request, "pages/dashboard.html", context={'plot_div': plot_div})
 
 
 def home(request):
